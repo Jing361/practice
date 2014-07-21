baeb338c4a332ef2c0cc8ecb37b742d5cae1077b
Chris Samuelson <chris.sam55@gmail.com>
2014-07-21 13:25:02 -0400

    Fixed percept generation again.

--- wa-tor/WatorWorld.cc
+++ wa-tor/WatorWorld.cc
@@ -23,6 +23,34 @@ void WatorWorld::set(unsigned int x, unsigned int y, Entity* newEnt){
   this->universe[x][y] = newEnt;
 }
  
-Entity** getPercepts(unsigned int cX, unsigned int cY, unsigned int sX, unsigned int sY){
+Entity*** getPercepts(unsigned int cX, unsigned int cY, unsigned int sX, unsigned int sY){
+  Entity*** percepts = new Entity**[sX];
+  for(int i = 0; i < sY; ++i){
+    percepts[i] = new Entity*[sY];
+  }
+  for(int k = cX - (sX / 2); k < (cX + (sX / 2)); ++k){
+    for(int l = cY - (sY / 2); l < (cY + (sY / 2)); ++l){
+      unsigned int x;
+      unsigned int y;
+      unsigned int xt = (1 + k) % this->height;
+      unsigned int yt = (1 + l) % this->width;
+      //Account for negative world indexes
+      if(xt < 0){
+        x = this->height + xt;
+      } else {
+        x = xt;
+      }
+      if(yt < 0){
+        y = this->width + yt;
+      } else {
+        y = yt;
+      }
+      //loop control variable is a modifier to the central cell
+      //to generalize: center is half of percept range
+      //    modifier is negative half of percept range
+      //    up to positive half of percept range
+      percepts[x][y] = this->universe[i][j];
+    }
+  }
 }
 
