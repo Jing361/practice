4e64c2793d8940126950880370ab9ba827b6f4b3
Chris Samuelson <chris.sam55@gmail.com>
2016-01-09 18:06:00 -0500

    Encapsulated unbuffered quit signal as class.

--- 2DEngine/main.cc
+++ 2DEngine/main.cc
@@ -1,78 +1,9 @@
 #include<iostream>
 #include<string>
 #include<chrono>
-#include<thread>
-#include<termios.h>
-#include<unistd.h>
+#include"engine.hh"
 #include"image.hh"
 #include"buffer.hh"
-
-class bufferedIO{
-private:
-  bool m_isBuffer = true;
-
-public:
-  void turnOn(){
-    struct termios t;
-    //get the current terminal I/O structure
-    tcgetattr(0, &t);
-    //Manipulate the flag bits to do what you want it to do
-    t.c_lflag |= ICANON;
-    //Apply the new settings
-    tcsetattr(0, TCSANOW, &t);
-    m_isBuffer = true;
-  }
-
-  void turnOff(){
-    struct termios t;
-    //get the current terminal I/O structure
-    tcgetattr(0, &t);
-    //Manipulate the flag bits to do what you want it to do
-    t.c_lflag &= ~ICANON;
-    //Apply the new settings
-    tcsetattr(0, TCSANOW, &t);
-    m_isBuffer = false;
-  }
-
-  void toggleBuffering(){
-    if(m_isBuffer){
-      turnOff();
-    } else {
-      turnOn();
-    }
-  }
-
-};
-
-class engine{
-private:
-  bool m_end = false;
-  std::thread wait;
-
-public:
-  engine():
-    wait(&engine::killThread, this){
-  }
-
-  virtual ~engine(){
-    wait.join();
-  }
-
-  void killThread(){
-    bufferedIO b;
-    b.turnOff();
-    char c = ' ';
-    while(c != 'q'){
-      c = getchar();
-    }
-    m_end = true;
-    b.turnOn();
-  }
-
-  bool shouldQuit(){
-    return m_end;
-  }
-};
 
 int main(){
   buffer<20, 20> frame;
