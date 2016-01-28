ee9f5e93470aaf865d16c1c1afb94f5d6ee8ff66
Chris Samuelson <chris.sam55@gmail.com>
2016-01-27 23:15:50 -0500

    Created tar file splitter.

---
+++ tarUtil/main.cc
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<fstream>
+
+int main(int argc, char** argv){
+  if(argc != 2){
+    std::cout << "Incorrect number of arguments.\nCall as " << argv[0] << " tarfile." << std::endl;
+  }
+  std::ifstream file(argv[1], std::ios::binary | std::ios::ate);
+  int tarSize = file.tellg();
+  char* mem = new char[tarSize];
+  int idx = 0;
+
+  file.seekg(0);
+  file.read(mem, tarSize);
+
+  for(int j = 0; j < 2; ++j){
+    std::string fileName(&mem[idx], 100);
+    std::ofstream out(fileName);
+    std::string sizeStr(&mem[idx + 124], 12);
+    int fileSize = stoi(sizeStr, 0, 8);
+    idx += 512;
+
+    for(int i = idx; i < idx + fileSize; ++i){
+      out << mem[i];
+    }
+    out << std::flush;
+
+    idx += fileSize;
+    while(idx % 512 != 0){
+      ++idx;
+    }
+  }
+
+  delete mem;
+  return 0;
+}
+
