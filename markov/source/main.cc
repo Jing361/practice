500fd2b82f32b33b0bcb3f92e6e6a99a3ed0b4cf
Chris Samuelson <chris.sam55@gmail.com>
2017-09-27 11:57:21 -0400

    Moved argument parser to submodule, and updated makefiles

--- markov/source/main.cc
+++ markov/source/main.cc
@@ -15,9 +15,9 @@ using namespace std;
 int main( int argc, const char** argv ){
   argparse ap;
 
-  ap.add_argument( "-data", "data/sherlock.txt", 1 );
-  ap.add_argument( "-words", "0", 1 );
-  ap.add_argument( "-sentence", "1", 1 );
+  ap.add_argument( "-data", "data/sherlock.txt", "", 1 );
+  ap.add_argument( "-words", "0", "", 1 );
+  ap.add_argument( "-sentence", "1", "", 1 );
 
   ap.parse_args( argc, argv );
 
