fccdabdc4d96c56b3ece653c0068a4f1c6d084e2
Chris Samuelson <chris.sam55@gmail.com>
2018-03-06 14:26:13 +0000

    created basic_gEntity for a basic graphic entity implementation
    Also laid ground for a basic_entity that can be used in graphics or physics

--- 2DEngine/header/entity.hh
+++ 2DEngine/header/entity.hh
@@ -4,11 +4,12 @@
 #include<vector>
 
 #include<graphics.hh>
+#include<physics.hh>
 #include"shared_types.hh"
 
 class basic_pEntity{
 private:
-  std::vector<basic_pEntity> mParts;
+  std::vector<pEntity> mParts;
   simple_box mHitBox;
   double mMass;
   vec2 mNetForce;
@@ -22,6 +23,11 @@ public:
   //! @todo use unit datastructure
   basic_pEntity( vec2 pos = {0, 0}, vec2 vel = {0, 0}, vec2 acc = {0, 0},
                  double mass = 1.0, simple_box hit_box = {{0.0, 0.0}, {1.0, 1.0}} );
+
+  void
+  add_part( pEntity ent ){
+    mParts.push_back( ent );
+  }
 
   simple_box&
   get_bounding_box();
@@ -52,6 +58,77 @@ public:
 
   vec2
   get_net_force() const;
+};
+
+class basic_gEntity{
+private:
+  std::vector<gEntity> mParts;
+  gEntity mEntity;
+
+public:
+  basic_gEntity( gEntity ent );
+
+  basic_gEntity( const basic_gEntity& ) = default;
+
+  basic_gEntity( basic_gEntity&& ) = default;
+
+  basic_gEntity&
+  operator=( const basic_gEntity& ) = default;
+
+  basic_gEntity&
+  operator=( basic_gEntity&& ) = default;
+
+  void
+  add_part( gEntity );
+
+  void
+  draw( graphics& gfx ) const;
+};
+
+class basic_entity{
+private:
+  std::vector<basic_entity> mParts;
+  gEntity mEntity;
+  simple_box mHitBox;
+  double mMass;
+  vec2 mNetForce;
+  vec2 mPosition;
+  vec2 mVelocity;
+  vec2 mAcceleration;
+
+public:
+  simple_box&
+  get_bounding_box();
+
+  const simple_box&
+  get_bounding_box() const;
+
+  vec2&
+  get_velocity();
+
+  vec2&
+  get_acceleration();
+
+  vec2&
+  get_position();
+
+  double&
+  get_mass();
+
+  double
+  get_total_mass() const;
+
+  void
+  tick( double diff );
+
+  void
+  apply_force( vec2 force );
+
+  vec2
+  get_net_force() const;
+
+  void
+  draw( graphics& gfx ) const;
 };
 
 #endif
--- 2DEngine/source/entity.cc
+++ 2DEngine/source/entity.cc
@@ -1,5 +1,9 @@
 #include<entity.hh>
 
+/*****************
+ * basic_pEntity *
+ *****************/
+
 basic_pEntity::basic_pEntity( const basic_pEntity& other )
   : mParts( other.mParts )
   , mHitBox( other.mHitBox )
@@ -73,5 +77,75 @@ basic_pEntity::apply_force( vec2 force ){
 vec2
 basic_pEntity::get_net_force() const{
   return mNetForce;
+}
+
+/*****************
+ * basic_gEntity *
+ *****************/
+
+basic_gEntity::basic_gEntity( gEntity ent )
+  : mEntity( ent ){
+}
+
+void
+basic_gEntity::add_part( gEntity ent ){
+  mParts.push_back( ent );
+}
+
+void
+basic_gEntity::draw( graphics& gfx ) const{
+  for( auto part : mParts ){
+    part.draw( gfx );
+  }
+
+  mEntity.draw( gfx );
+}
+
+/****************
+ * basic_entity *
+ ****************/
+
+simple_box&
+basic_entity::get_bounding_box(){
+}
+
+const simple_box&
+basic_entity::get_bounding_box() const{
+}
+
+vec2&
+basic_entity::get_velocity(){
+}
+
+vec2&
+basic_entity::get_acceleration(){
+}
+
+vec2&
+basic_entity::get_position(){
+}
+
+double&
+basic_entity::get_mass(){
+}
+
+double
+basic_entity::get_total_mass() const{
+}
+
+void
+basic_entity::tick( double diff ){
+}
+
+void
+basic_entity::apply_force( vec2 force ){
+}
+
+vec2
+basic_entity::get_net_force() const{
+}
+
+void
+basic_entity::draw( graphics& gfx ) const{
 }
 
